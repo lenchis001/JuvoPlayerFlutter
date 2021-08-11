1e5743c4e4835bca187cde8f88adb35573db91c2
Liavontsi Khachuyeu <liavontsi_khachuyeu@epam.com>
2021-08-11 18:24:32 +0300

    Playback of clean content works okay on TV.Multimedia player

--- example/tizen/JuvoPlayerWrapper.cs
+++ example/tizen/JuvoPlayerWrapper.cs
@@ -108,7 +108,7 @@ namespace Runner{
                 /////////////Clean contents////////////////////
                 //var url = "http://yt-dash-mse-test.commondatastorage.googleapis.com/media/car-20120827-manifest.mpd";
                 //var url = "https://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd";
-                //var url = "https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd";
+                var url = "https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd";
                 //var url = "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4";
                 //var url = "http://wowzaec2demo.streamlock.net/live/bigbuckbunny/manifest_mvtime.mpd";
 
@@ -123,22 +123,22 @@ namespace Runner{
                 //var license = "https://widevine-proxy.appspot.com/proxy";
                 //var url = "https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears_uhd.mpd";
                 //var license = "https://proxy.uat.widevine.com/proxy?provider=widevine_test";
-                var url = "https://media.axprod.net/TestVectors/v7-MultiDRM-SingleKey/Manifest_1080p.mpd";
-                var license = "https://drm-widevine-licensing.axtest.net/AcquireLicense";
+                //var url = "https://media.axprod.net/TestVectors/v7-MultiDRM-SingleKey/Manifest_1080p.mpd";
+                //var license = "https://drm-widevine-licensing.axtest.net/AcquireLicense";
 
                 //////The TV platform MediaPlayer (URL data source only).
-                //platformPlayer = new TVMultimedia.Player { Display = new Display(window) };
-                //await PlayPlatformMediaClean(url, platformPlayer);
+                platformPlayer = new TVMultimedia.Player { Display = new Display(window) };
+                await PlayPlatformMediaClean(url, platformPlayer);
                 //await PlayPlatformMediaDRMed(url, license, platformPlayer);
 
                 //////The JuvoPlayer backend (elementary stream data source).
                 
-                juvoPlayer = new PlayerServiceProxy<PlayerServiceImpl>();
-                juvoPlayer.SetWindow(window);
+                //juvoPlayer = new PlayerServiceProxy<PlayerServiceImpl>();
+                //juvoPlayer.SetWindow(window);
                 //PlayJuvoPlayerClean(url, juvoPlayer);
                 
                 //PlayJuvoPlayerDRMed(url, license, "playready", juvoPlayer);
-                PlayJuvoPlayerDRMed(url, license, "widevine", juvoPlayer);
+                //PlayJuvoPlayerDRMed(url, license, "widevine", juvoPlayer);
             }
         
             Logger.Info("Before Play().");
